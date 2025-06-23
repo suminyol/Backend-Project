fa1a8521c8885841d3ecceef5f4bb9a17b7e67fc
Suminder <sumi786nyol@gmail.com>
2025-06-24 04:33:10 +0530

    Utils code (expess,cookie)

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+.then((e)=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT} `);
+        
+    })
+
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!",err);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app=express();
+(async()=>{
+    try {
+         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         app.on("error",(error)=>{
+            console.log("Error in server: ", error);
+            throw error;
+         })
+         app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+         })
+    } catch (error) {
+        console.log("ERROR: ", error);
+        
+    }
+})()
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+
+const asyncHandler=(requestHanler)=>{
+    (req,res,next) => {
+        Promise.resolve(requestHanler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+// const asyncHandler = (fn) =>  async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code||500).json({
+//             success: false,
+//             message:err.message
+//         })
+//     }
+// }
