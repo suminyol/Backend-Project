6bfde90fc8ce8c31a3884d65a6fa21e1bf014657
Suminder <sumi786nyol@gmail.com>
2025-06-25 07:46:35 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,14 +19,14 @@ const registerUser= asyncHandler(async(req,res)=>{
     const {fullName,email,username,password}=req.body
 
    
-
+        
    if([fullName,email,username,password].some((field)=>(
     field?.trim()===""
    ))){
     throw new ApiError(400,"All fields are required")
    }
    
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
     $or :[{ username }, { email }]
    })
    
@@ -37,8 +37,13 @@ const registerUser= asyncHandler(async(req,res)=>{
    }
 
    const avatarLocalPath=req.files?.avatar[0]?.path;
-   const coverImageLocalPath=req.files?.coverImage[0]?.path;
-
+   // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    //console.log(req.files);
+    let coverImageLocalPath
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath= req.files.coverImage[0].path;
+    }
+    
    if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
    }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,13 +6,13 @@ import {upload} from "../middlewares/multer.middleware.js"
 const router=Router();
 
 router.route("/register").post(
-     upload.fields({
+     upload.fields([{
         name:"avatar",
         maxCount:1
      },{
         name:"coverImage",
         maxCount:1
-     })
+     }])
     ,registerUser);
 
 export default router
